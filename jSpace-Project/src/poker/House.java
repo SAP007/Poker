aefe27daa97560e40f88033015bfae644139011e
Sofie-Amalie <s174255@student.dtu>
2020-01-12 14:10:59 +0100

    made connection between House and Player, added constructor for Player

--- jSpace-Project/src/poker/House.java
+++ jSpace-Project/src/poker/House.java
@@ -1,5 +1,55 @@
+
 package poker;
+
+//
+//import java.io.BufferedReader;
+//import java.io.IOException;
+//import java.io.InputStreamReader;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.jspace.FormalField;
+import org.jspace.SequentialSpace;
+import org.jspace.SpaceRepository;
 
 public class House {
 
+	public static void main(String[] args) {
+		try {
+
+			// BufferedReader input = new BufferedReader(new
+			// InputStreamReader(System.in));
+
+			// Create a repository
+			SpaceRepository repository = new SpaceRepository();
+
+			// Create a local space for the chat messages
+			SequentialSpace lobby = new SequentialSpace();
+
+			// Add the space to the repository
+			repository.add("lobby", lobby);
+
+			// Default value
+			String uri = "tcp://127.0.0.1:9001/?keep";
+
+			// Open a gate
+			URI myUri = new URI(uri);
+			String gateUri = "tcp://" + myUri.getHost() + ":" + myUri.getPort() + "?keep";
+			System.out.println("Opening repository gate at " + gateUri + "...");
+			repository.addGate(gateUri);
+
+			//
+			while (true) {
+				Object[] t = lobby.get(new FormalField(String.class), new FormalField(Integer.class),
+						new FormalField(Integer.class),new FormalField(Integer.class));
+				System.out.println(t[0] + ": Balance: " + t[1] + " playerId: " + t[2] + " " + "bet: " + t[3]);
+			}
+
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} catch (URISyntaxException e) {
+
+			e.printStackTrace();
+		}
+	}
 }
--- jSpace-Project/src/poker/Player.java
+++ jSpace-Project/src/poker/Player.java
@@ -1,5 +1,74 @@
 package poker;
 
-public class Player {
+import org.jspace.RemoteSpace;
+import org.jspace.Tuple;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.UnknownHostException;
+
+public class Player {
+	// fields
+	private int balance;
+	private String name;
+	private static int playerId = 0;
+
+	// default constructor
+	public Player() {
+		balance = 0;
+		name = "";
+		playerId = 0;
+	}
+
+	//Constructor
+	public Player( String name,int balance, int playerId) {
+		this.name = name;
+		this.balance = balance;
+		this.playerId= playerId + 1;
+	}
+
+	public static void main(String[] args) throws InterruptedException {
+
+		try {
+
+			BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+
+			// Set the URI of the chat space
+			// Default value
+			System.out.print("Enter URI of the chat server or press enter for default: ");
+			String uri = input.readLine();
+			// Default value
+			if (uri.isEmpty()) {
+				uri = "tcp://127.0.0.1:9001" + "/lobby?keep";
+			}
+
+			// Connect to the remote chat space
+			System.out.println("Connecting to chat space " + uri + "...");
+			RemoteSpace lobby = new RemoteSpace(uri);
+
+			// create your player
+			System.out.print("Enter your name: ");
+			String name = input.readLine();
+			System.out.print("Enter your balance: ");
+			int balance = Integer.parseInt(input.readLine());
+
+			Player player = new Player((String) name , (int) balance, Player.playerId);
+			
+
+			// Keep sending user name and call, raise fold
+			System.out.println("make a bet...");
+			while (true) {
+				int bet = Integer.parseInt(input.readLine());
+				lobby.put(player.name, player.balance, Player.playerId, bet);
+			}
+
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
