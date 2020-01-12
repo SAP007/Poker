fa047aaed6a7fea17d84cf40d12b8dd6ae6b45fe
OliKarason <OliKarason@Oli>
2020-01-12 21:37:08 +0100

    Added rule determination for flush,multOfAkind,fullHouse, new enum Suit

--- jSpace-Project/src/poker/Card.java
+++ jSpace-Project/src/poker/Card.java
@@ -2,4 +2,20 @@ package poker;
 
 public class Card {
 
+	Suit suit;
+	int number;
+	
+	public Card(Suit suit, int number) {
+		this.suit = suit;
+		this.number = number;
+	}
+	
+	public Suit getSuit() {
+		return suit;
+	}
+	
+	public int getNumber() {
+		return number;
+	}
+	
 }
