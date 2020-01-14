d39516a516b5c03f80c68c36945c32862d4653e8
Sofie-Amalie <s174255@student.dtu>
2020-01-14 14:09:20 +0100

    changed the queryAll to match pattern

--- jSpace-Project/src/poker/House.java
+++ jSpace-Project/src/poker/House.java
@@ -13,14 +13,13 @@ import org.jspace.FormalField;
 //import java.io.InputStreamReader;
 
 public class House {
-	
+
 	public static void main(String[] args) throws InterruptedException {
 		spaceRepo();
 		System.out.println("SpaceRepo created");
 		deckSpace();
 		System.out.println("deckSpace created");
 		createLobby();
-
 	}
 
 	static SpaceRepository spaceRepo = new SpaceRepository();
@@ -43,23 +42,22 @@ public class House {
 	}
 
 	public static void createLobby() throws InterruptedException {
-		
-			//creates a lobby for players 
-			SequentialSpace lobby = new SequentialSpace();
-			// Add the space to the repository
-			spaceRepo.add("lobby", lobby);
-			
 
-			while (true) {
-				System.out.println("get from client");
-				Object[] t = lobby.get(new FormalField(String.class), new FormalField(Integer.class), new FormalField(Integer.class), new FormalField(Integer.class));
-				lobby.put(t[0], t[3] );
-				
-				
-				System.out.println(t[0] + ": Balance: " + t[1] + " playerId: " + t[2] + "bet: " + t[3]);
-				System.out.println("done");
-			}
-			
-		
+		// creates a lobby for players
+		SequentialSpace lobby = new SequentialSpace();
+		// Add the space to the repository
+		spaceRepo.add("lobby", lobby);
+
+		while (true) {
+			System.out.println("get from client");
+			Object[] t = lobby.get(new FormalField(String.class), new FormalField(Integer.class),
+					new FormalField(Integer.class), new FormalField(Integer.class));
+			lobby.put(t[0], t[3]);
+
+			System.out.println(t[0] + ": Balance: " + t[1] + " playerId: " + t[2] + "bet: " + t[3]);
+			System.out.println("done");
+		}
 	}
 }
+
+
--- jSpace-Project/src/poker/Player.java
+++ jSpace-Project/src/poker/Player.java
@@ -16,13 +16,14 @@ public class Player {
 	private int balance;
 	private String name;
 	private int playerId;
+
 	// default constructor
 	public Player() {
 		balance = 0;
 		name = "";
 	}
 
-	//Constructor
+	// Constructor
 	public Player(String name, int balance) {
 		this.name = name;
 		this.balance = balance;
@@ -35,35 +36,31 @@ public class Player {
 			BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
 
 			// Set the URI of the chat space
-	
+
 			String uri = "tcp://localhost:9003/lobby?keep";
 
 			// Connect to the remote chat space
 			System.out.println("Connecting to poker lobby " + uri + "...");
 			RemoteSpace lobby = new RemoteSpace(uri);
 
-			// create your player
+			// create a player
 			System.out.print("Enter your name: ");
 			String name = input.readLine();
 			System.out.print("Enter your balance: ");
 			int balance = Integer.parseInt(input.readLine());
 
-			Player player = new Player((String) name , (int) balance);
-			
+			Player player = new Player((String) name, (int) balance);
 
-			// Keep sending user name and call, raise fold
+			// Keep sending username and call, raise fold
 			System.out.println("make a bet...");
 			while (true) {
 				int bet = Integer.parseInt(input.readLine());
-				lobby.put(player.name, player.balance, player.playerId, bet);				
 				
-				List<Object[]> info = lobby.queryAll(new FormalField(String.class), new FormalField(Integer.class));
-				System.out.println("plays made:");
-				for(Object[] playerInfo : info){
-		            System.out.println("player: " + playerInfo[0] + " placed: " + playerInfo[1]);
+				//(id, navn, balance, kort1, kort2, r positive number /c zero /f -1 )
+				lobby.put(player.name, player.balance, player.playerId, bet);
 
-				}
-				
+				new Thread(new displayHandler(uri, name)).start();
+			
 			}
 
 		} catch (UnknownHostException e) {
@@ -74,4 +71,66 @@ public class Player {
 			e.printStackTrace();
 		}
 	}
+
+}
+
+class displayHandler implements Runnable {
+	private RemoteSpace lobby;
+	private RemoteSpace board;
+
+	public displayHandler(String lobbyUrl, String boardUrl) throws UnknownHostException, IOException {
+		lobby = new RemoteSpace(lobbyUrl);
+		board = new RemoteSpace(boardUrl);
+
+	}
+
+	public void run() {
+		try {
+
+			// keep updating player info and printing them
+			while (true) {
+				System.out.println();
+				System.out.println();
+				System.out.println();
+				System.out.println();
+				System.out.println();
+				System.out.println();
+				System.out.println();
+				System.out.println();
+				System.out.println();
+				System.out.println();
+
+				List<Object[]> boardInfo = board.queryAll(
+						new FormalField(Integer.class),  // first card
+						new FormalField(Integer.class),  // second card
+						new FormalField(Integer.class),  // third card
+						new FormalField(Integer.class),  // fourth card
+						new FormalField(Integer.class),  // fifth card
+						new FormalField(Integer.class)); // pot
+				for (Object[] boardState : boardInfo) {
+					System.out.println("[ " + boardState[0] + " ] " +"[ " + boardState[1] + " ] " +"[ " + boardState[2] + " ] " +"[ " + boardState[3] + " ] " +"[ " + boardState[4] + " ] " +" " + boardState[5] + " $ " );
+				}
+
+				List<Object[]> info = board.queryAll(
+						new FormalField(Integer.class), // Player Id 
+						new FormalField(String.class),  // player name
+						new FormalField(Integer.class), // players balance 
+						new FormalField(Integer.class), // first card
+						new FormalField(Integer.class), // second card
+						new FormalField(Integer.class)  // Raise, check, Fold
+						);
+				System.out.println("plays made:");
+				for (Object[] playerInfo : info) {
+					System.out.println("player id: " + playerInfo[0] + " name: " +
+							playerInfo[1] + "balance: " + playerInfo[2] + " firstCard: " + 
+							playerInfo[3] + " secondCard: " + playerInfo[4] + " R/C/F: " + playerInfo[5] );
+				}
+
+				Thread.sleep(500);
+			}
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
 }
