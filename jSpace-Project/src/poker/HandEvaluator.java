c2957609db00e03b9c44f56417baf043a6980128
OliKarason <s174269@student.dtu.dk>
2020-01-14 11:50:31 +0100

    Moved more methods to HandEvaluator

--- jSpace-Project/src/poker/HandEvaluator.java
+++ jSpace-Project/src/poker/HandEvaluator.java
@@ -5,9 +5,110 @@ public class HandEvaluator {
 	public static final int HANDSIZE = 7;
 	
 	
+	
+	
+	
+	public static Card[] getFlushHand(Card[] hand) throws Exception {
+
+		Card[] actualHand = new Card[5];
+
+		int numOfClubs, numOfDiamonds, numOfHearts, numOfSpades;
+		numOfClubs = 0;
+		numOfDiamonds = 0;
+		numOfHearts = 0;
+		numOfSpades = 0;
+
+		Card[] clubHand = new Card[5];
+		Card[] diamondHand = new Card[5];
+		Card[] heartHand = new Card[5];
+		Card[] spadeHand = new Card[5];
+
+		Suit currSuit;
+		Card currCard;
+
+		// return false if one suit does not match
+		for (int i = 1; i < HANDSIZE; i++) {
+
+			currCard = hand[i];
+
+			// get suit of current card
+			currSuit = currCard.getSuit();
+
+			// increment counter of appropriate suit
+			if (currSuit == Suit.CLUBS) {
+
+				if (numOfClubs >= 5)
+					clubHand = insertIfLarger(clubHand, currCard);
+
+				else {
+					clubHand[numOfClubs] = currCard;
+					numOfClubs++;
+				}
+			}
+
+			else if (currSuit == Suit.DIAMONDS) {
+
+				if (numOfDiamonds >= 5)
+					diamondHand = insertIfLarger(diamondHand, currCard);
+
+				else {
+					diamondHand[numOfDiamonds] = currCard;
+					numOfDiamonds++;
+				}
+			}
+
+			else if (currSuit == Suit.HEARTS) {
+
+				if (numOfHearts >= 5)
+					heartHand = insertIfLarger(heartHand, currCard);
+
+				else {
+					heartHand[numOfHearts] = currCard;
+					numOfHearts++;
+				}
+			}
+
+			else if (currSuit == Suit.SPADES) {
+
+				if (numOfSpades >= 5)
+					spadeHand = insertIfLarger(spadeHand, currCard);
+
+				else {
+					spadeHand[numOfSpades] = currCard;
+					numOfSpades++;
+				}
+			}
+
+			// if no counter can be incremented, throw exception
+			else {
+				throw new Exception("Invalid suit value: " + currSuit);
+			}
+
+		}
+
+		if (numOfClubs >= 5) {
+			actualHand = clubHand;
+		} else if (numOfDiamonds >= 5) {
+			actualHand = diamondHand;
+		} else if (numOfHearts >= 5) {
+			actualHand = heartHand;
+		}
+
+		else if (numOfSpades >= 5) {
+			actualHand = spadeHand;
+		}
+
+		return actualHand;
+
+	}
+	
+	
+	
+	
+	
+	
 	public static Card[] getStraightHand(Card[] hand) {
 
-		int consNums = 1;
 
 		hand = shellsort(hand);
 
@@ -26,7 +127,6 @@ public class HandEvaluator {
 			// if nextNum is exactly one greater than currNum, add to actualHand
 			if (nextNum == currNum + 1) {
 
-				consNums++;
 
 				actualHand[actualIndex % 5] = hand[i];
 				actualIndex++;
@@ -37,7 +137,6 @@ public class HandEvaluator {
 				continue;
 
 			else {
-				consNums = 1;
 
 				for (int j = 0; j < 5; j++) {
 					actualHand[j] = null;
@@ -65,6 +164,31 @@ public class HandEvaluator {
 	
 	
 	
+	// used to replace smaller flush cards with greater ones
+	public static Card[] insertIfLarger(Card[] actualHand, Card card) {
+
+		int min = 15;
+		int currNum;
+		Card minCard = null;
+		int minIndex = -1;
+
+		for (int i = 0; i < actualHand.length; i++) {
+
+			currNum = actualHand[i].getNumber();
+
+			if (currNum < min) {
+				min = currNum;
+				minCard = actualHand[i];
+				minIndex = i;
+			}
+		}
+
+		if (minCard.getNumber() < card.getNumber() && minIndex > -1) {
+			actualHand[minIndex] = card;
+		}
+
+		return actualHand;
+	}
 	
 	
 	
