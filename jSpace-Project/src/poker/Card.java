64be85dd2a216e7ca4098e1775bbaac00dbbe2b0
OliKarason <s174269@student.dtu.dk>
2020-01-22 11:37:59 +0100

    reverted Player

--- jSpace-Project/src/poker/Card.java
+++ jSpace-Project/src/poker/Card.java
@@ -9,8 +9,9 @@ import org.jspace.RemoteSpace;
 
 public class Card {	
 
-	Suit suit;
-	int number;
+	private Suit suit;
+	private int number;
+  
 	public Card(){
 		String suit;
 		int value;
@@ -28,28 +29,42 @@ public class Card {
 	public int getNumber() {
 		return number;
 	}
+  
+  public String toString() {
+		String suitChar;
+		// TODO: change suitChar to unicode for suits
+
+		if (suit == Suit.CLUBS)
+			suitChar = "\u2663";
+
+		else if (suit == Suit.DIAMONDS)
+			suitChar = "\u2662";
+
+		else if (suit == Suit.HEARTS)
+			suitChar = "\u2661";
+
+		else
+			suitChar = "\u2660";
+
+		return "[ " + number + suitChar + " ]";
+
+	}
 	
 	
 	public static void addToDeck() throws InterruptedException, UnknownHostException, IOException {
 		//Need to create a check if the space exists.
-		String deckUri = "tcp://localhost:9002/deck?keep";
+		String deckUri = "tcp://localhost:9003/deck?keep";
 		RemoteSpace deckSpace = new RemoteSpace(deckUri);
 	for (int i=1; i<=4; i++) {
 		for(int j=2; j<=14; j++) {
 			deckSpace.put(i, j); //tuple,
 		}
 	}
-	Thread.sleep(600);
-	Object[] t = deckSpace.query(new FormalField(Integer.class), new FormalField(Integer.class));
-	System.out.println(deckSpace.size());
-	Object[] ts = deckSpace.get(new FormalField(Integer.class), new FormalField(Integer.class));
-	System.out.println(ts[0]);
-	System.out.println(ts[1]);
 	}	
 	
 	
 	public static void main(String[] args) throws IOException, IOException, InterruptedException {
-	String deckUri = "tcp://localhost:9002/deck?keep";
+	String deckUri = "tcp://localhost:9003/deck?keep";
 	RemoteSpace deckSpace = new RemoteSpace(deckUri);
 	int numberOfCards = 52;
 	
@@ -59,3 +74,4 @@ public class Card {
 			
 				
 }
+
---
+++ jSpace-Project/src/poker/HandFinder.java
@@ -0,0 +1,369 @@
+
+package poker;
+
+public class HandFinder {
+
+	public static final int HANDSIZE = 7;
+
+	static Card[] findStraightFlush(Card[] hand) {
+
+		hand = RulesAux.sortHand(hand);
+		
+		Card[] actualHand = new Card[5];
+		int i = 6;
+
+		actualHand = findStraightHand(hand);
+		
+		while (i > 0) {
+			
+			if (isValidHand(actualHand)) {
+				if (isFlush(actualHand)) {
+					return actualHand;
+				}
+				else {
+					hand = RulesAux.removeLastCard(hand);
+					actualHand = findStraightHand(hand);
+				}
+			}
+			i--;
+			
+		}
+
+		return actualHand;
+
+	}
+
+	static Card[] findTwoPairs(Card[] hand) {
+
+		Card[] actualHand = new Card[5];
+		hand = RulesAux.sortHand(hand);
+
+		boolean kickerFound = false;
+
+		int pairVal = -1;
+		int pairsFound = 0;
+		int actualIndex = 0;
+
+		Card currCard = hand[hand.length - 1];
+
+		for (int i = hand.length - 2; i >= 0; i--) {
+			Card nextCard = hand[i];
+
+			int currVal = currCard.getNumber();
+			int nextVal = nextCard.getNumber();
+
+			/*
+			 * currCard and nextCard are equal, save them as pair and exclude that value
+			 * from being classified as pair later
+			 */
+			if (currVal == nextVal && currVal != pairVal && actualIndex < 5 && pairsFound < 2) {
+				actualHand[actualIndex] = currCard;
+				actualHand[actualIndex + 1] = nextCard;
+
+				pairVal = currVal;
+				actualIndex += 2;
+				pairsFound++;
+
+			}
+			/*
+			 * for two pairs there is room for one kicker, first one found will be the
+			 * highest and is saved
+			 */
+			else if (!kickerFound && actualIndex < 5 && currVal != pairVal) {
+				actualHand[actualIndex] = currCard;
+				actualIndex++;
+				kickerFound = true;
+			}
+			currCard = nextCard;
+
+		}
+
+		return actualHand;
+
+	}
+
+	static Card[] findMultOfAKind(Card[] hand, int mult) {
+
+		Card[] actualHand = new Card[5];
+
+		int[] multTracker = RulesAux.makeMultTracker(hand);
+
+		int bestVal = -1;
+
+		for (int i = multTracker.length - 1; i >= 0; i--) {
+			int currMult = multTracker[i];
+			if (currMult >= mult) {
+
+				bestVal = i + 2;
+
+				break;
+			}
+
+		}
+
+		// if no better value is found, return invalid hand
+		if (bestVal == -1)
+			return actualHand;
+
+		// indicates number of cards in hand that are not part of combo
+		int remainder = actualHand.length - mult;
+
+		int actualIndex = actualHand.length - 1;
+		for (int i = hand.length - 1; i >= 0; i--) {
+			int currVal = hand[i].getNumber();
+			if (currVal == bestVal && actualIndex >= 0) {
+				actualHand[actualIndex] = hand[i];
+				actualIndex--;
+			} else if (remainder > 0 && actualIndex >= 0) {
+				actualHand[actualIndex] = hand[i];
+				actualIndex--;
+				remainder--;
+			}
+
+		}
+
+		return actualHand;
+	}
+
+	public static Card[] findFlushHand(Card[] hand) throws Exception {
+
+		Card[] actualHand = new Card[5];
+
+		int numOfClubs, numOfDiamonds, numOfHearts, numOfSpades;
+		numOfClubs = 0;
+		numOfDiamonds = 0;
+		numOfHearts = 0;
+		numOfSpades = 0;
+
+		Card[] clubHand = new Card[5];
+		Card[] diamondHand = new Card[5];
+		Card[] heartHand = new Card[5];
+		Card[] spadeHand = new Card[5];
+
+		Suit currSuit;
+		Card currCard;
+
+		// return false if one suit does not match
+		for (int i = 0; i < HANDSIZE; i++) {
+
+			currCard = hand[i];
+
+			// get suit of current card
+			currSuit = currCard.getSuit();
+
+			// increment counter of appropriate suit
+			if (currSuit == Suit.CLUBS) {
+
+				if (numOfClubs >= 5)
+					clubHand = insertIfLarger(clubHand, currCard);
+
+				else {
+					clubHand[numOfClubs] = currCard;
+					numOfClubs++;
+				}
+			}
+
+			else if (currSuit == Suit.DIAMONDS) {
+
+				if (numOfDiamonds >= 5)
+					diamondHand = insertIfLarger(diamondHand, currCard);
+
+				else {
+					diamondHand[numOfDiamonds] = currCard;
+					numOfDiamonds++;
+				}
+			}
+
+			else if (currSuit == Suit.HEARTS) {
+
+				if (numOfHearts >= 5)
+					heartHand = insertIfLarger(heartHand, currCard);
+
+				else {
+					heartHand[numOfHearts] = currCard;
+					numOfHearts++;
+				}
+			}
+
+			else if (currSuit == Suit.SPADES) {
+
+				if (numOfSpades >= 5)
+					spadeHand = insertIfLarger(spadeHand, currCard);
+
+				else {
+					spadeHand[numOfSpades] = currCard;
+					numOfSpades++;
+				}
+			}
+
+			// if no counter can be incremented, throw exception
+			else {
+				throw new Exception("Invalid suit value: " + currSuit);
+			}
+
+		}
+
+		if (numOfClubs >= 5) {
+			actualHand = clubHand;
+		} else if (numOfDiamonds >= 5) {
+			actualHand = diamondHand;
+		} else if (numOfHearts >= 5) {
+			actualHand = heartHand;
+		}
+
+		else if (numOfSpades >= 5) {
+			actualHand = spadeHand;
+		}
+
+		return actualHand;
+
+	}
+
+	static Card[] findFullHouse(Card[] hand) {
+
+		int[] multTracker = new int[12];
+		Card[] actualHand = new Card[5];
+
+		hand = RulesAux.sortHand(hand);
+
+		multTracker = RulesAux.makeMultTracker(hand);
+
+		boolean tripFound = false;
+		// find triplet
+		int triplet = -1;
+		int pair = -1;
+		for (int i = 11; i >= 0; i--) {
+			if (multTracker[i] >= 3 && !tripFound) {
+				triplet = i + 2;
+				tripFound = true;
+			} else if (multTracker[i] >= 2) {
+				pair = i + 2;
+			}
+		}
+
+		int actualIndex = 0;
+
+		for (int i = 6; i >= 0; i--) {
+
+			Card card = hand[i];
+
+			if ((card.getNumber() == pair || card.getNumber() == triplet) && actualIndex < 5) {
+				actualHand[actualIndex] = card;
+				actualIndex++;
+			}
+		}
+
+		return actualHand;
+
+	}
+
+	public static Card[] findStraightHand(Card[] hand) {
+
+		hand = RulesAux.sortHand(hand);
+
+		// currently considered number
+		int currNum = hand[0].getNumber();
+
+		Card[] actualHand = new Card[5];
+		actualHand[0] = hand[0];
+
+		// index reached in actual index
+		int actualIndex = 1;
+
+		for (int i = 1; i < hand.length; i++) {
+			int nextNum = hand[i].getNumber();
+
+			// if nextNum is exactly one greater than currNum, add to actualHand
+			if (nextNum == currNum + 1) {
+
+				actualHand[actualIndex % 5] = hand[i];
+				actualIndex++;
+
+			}
+
+			else if (nextNum == currNum)
+				continue;
+
+			// If nextNum is not in sequnce, and straight has not yet been found, reset
+			// actualHand
+			else if (actualIndex < 4) {
+				for (int j = 0; j < 5; j++) {
+					actualHand[j] = null;
+				}
+				actualHand[0] = hand[i];
+				actualIndex = 1;
+			}
+
+			currNum = nextNum;
+
+		}
+
+		return actualHand;
+
+	}
+
+	// used to replace smaller flush cards with greater ones
+	public static Card[] insertIfLarger(Card[] actualHand, Card card) {
+
+		int min = 15;
+		int currNum;
+		Card minCard = null;
+		int minIndex = -1;
+
+		for (int i = 0; i < actualHand.length; i++) {
+
+			currNum = actualHand[i].getNumber();
+
+			if (currNum < min) {
+				min = currNum;
+				minCard = actualHand[i];
+				minIndex = i;
+			}
+		}
+
+		if (minCard.getNumber() < card.getNumber() && minIndex > -1) {
+			actualHand[minIndex] = card;
+		}
+
+		return actualHand;
+	}
+
+	private static boolean isFlush(Card[] hand) {
+
+		// saves suit of first card in hand
+		Suit firstSuit = hand[0].getSuit();
+
+		// return false if one suit does not match
+		for (int i = 1; i < hand.length; i++) {
+			if (hand[i].getSuit() != firstSuit) {
+				return false;
+			}
+		}
+		return true;
+
+	}
+	
+	//returns false, if actualHand contains any null cards
+	public static boolean isValidHand(Card[] actualHand) {
+
+		for (int i = 0; i < 5; i++) {
+			if (actualHand[i] == null)
+				return false;
+		}
+		return true;
+
+	}
+
+	public static int findHighCard(Card[] hand) {
+
+		hand = RulesAux.sortHand(hand);
+		int sum = 0;
+
+		for (int i = hand.length - 1; i > 1; i--) {
+			sum = sum + (hand[i].getNumber() * i * i);
+		}
+
+		return sum;
+	}
+
+}
